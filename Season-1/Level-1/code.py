6e598fe3b03b3d38f1ca11a909bc8d706e651c4f
Thomas Simmer <37903526+thomassimmer@users.noreply.github.com>
2024-04-06 09:53:15 +0000

    S1.L1

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -18,16 +18,22 @@ Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
     net = 0
+    total_amount_payable = 0
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if -1e5 <= item.amount <= 1e5:
+                net += item.amount
         elif item.type == 'product':
             net -= item.amount * item.quantity
+            total_amount_payable += item.amount * item.quantity
         else:
             return "Invalid item type: %s" % item.type
 
-    if net != 0:
+    if total_amount_payable > 1e5:
+        return 'Total amount payable for an order exceeded'
+
+    if round(net, 2) != 0:        
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
         return "Order ID: %s - Full payment received!" % order.id
